95669bbcd5418b8ce8ddd9645a37e2187aa48d49
Alexander Schmid <mail@schmidalex.de>
2026-03-03 00:39:57 +0100

    Bug: Config zum Deployen hat nicht funktioniert.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,13 @@ import node from "@astrojs/node";
 
 export default defineConfig({
   output: "server",
+  site: "https://alexschmid.dev",
   adapter: node({
     mode: "standalone",
   }),
+  server: {
+    allowedHosts: ["schmidalex.de", "alexschmid.dev"],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
